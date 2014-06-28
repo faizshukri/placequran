d21bfce25119830005929ea8abb47956eba8decf
Faiz Shukri <faizshukri90@gmail.com>
2014-06-28 16:09:41 +0800

    Increase referee column length

--- app/database/migrations/2014_03_21_175132_create_log_table.php
+++ app/database/migrations/2014_03_21_175132_create_log_table.php
@@ -19,7 +19,7 @@ class CreateLogTable extends Migration {
 		    $table->integer('surah');
 		    $table->string('verses', 100);
 		    $table->string('translation', 30);
-		    $table->string('referer', 255)->nullable();
+		    $table->string('referer', 1000)->nullable();
 		    $table->string('ip_address', 50);
 		    $table->string('country_code', 20);
 		    $table->string('browser', 25);
